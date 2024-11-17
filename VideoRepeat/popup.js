7c751003a3c33393a2d78e77e0b87683a4df0e98
TuanMinh7979 <minhtuanto179@gmail.com>
2024-11-17 17:04:50 +0700

    add storage

--- VideoRepeat/popup.js
+++ VideoRepeat/popup.js
@@ -1,9 +1,24 @@
 
+
 document.addEventListener("DOMContentLoaded", function () {
+  chrome.storage.local.get(['interval'], function (result) {
+    document.getElementById('interval').value = result.interval
+  });
+  chrome.storage.local.get(['repeatCount'], function (result) {
+    document.getElementById('repeatCount').value = result.repeatCount
+  });
+
+
   document.getElementById('start').onclick = function () {
+
     const interval = parseInt(document.getElementById('interval').value, 10);
     const repeatCount = parseInt(document.getElementById('repeatCount').value, 10);
+    chrome.storage.local.set({ interval }, function () {
 
+    });
+    chrome.storage.local.set({ repeatCount }, function () {
+
+    });
 
     // Gửi tham số tới content.js
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
